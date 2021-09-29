d86a210ac016f9e0822cc8203b404adcecc43070
jackpham07 <jack.pham@grapecity.com>
2021-09-29 20:57:09 +0700

    update user story #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FilterComponent } from './components/filter/filter.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SubmitButtonComponent } from './components/submit-button/submit-button.component';
+import { TransactionItemComponent } from './components/transaction-item/transaction-item.component';
+
+const COMPONENTS = [
+  FooterComponent,
+  LogoComponent,
+  SubmitButtonComponent,
+  FilterComponent,
+  TransactionItemComponent,
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    COMPONENTS
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  exports: COMPONENTS,
   providers: [],
   bootstrap: [AppComponent]
 })
